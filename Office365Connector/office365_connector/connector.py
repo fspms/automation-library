219b1caf7c657d0cc49f0a1cb1d705629241508c
Alexandre Léonardi <alexandre.leonardi@sekoia.io>
2023-10-30 16:37:30 +0000

    Finish base methods

--- Office365Connector/office365_connector/connector.py
+++ Office365Connector/office365_connector/connector.py
@@ -1,9 +1,44 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from functools import lru_cache
+from threading import Event, Thread
 from time import sleep
+import time
 import uuid
 from sekoia_automation.connector import Connector
+from office365_connector.errors import FailedToActivateO365Subscription
+from office365_connector.office365_api import Office365API
 from office365_connector.settings import Office365IntakeSettings
 from pathlib import Path
+
+from prometheus_client import Counter, Histogram
+
+EVENTS_CACHE = {}
+
+# Declare prometheus metrics
+prom_namespace = "sicconfapi_intakes"
+
+OUTCOMING_EVENTS = Counter(
+    name="forwarded_events",
+    documentation="Number of events forwarded to SEKOIA.IO",
+    namespace=prom_namespace,
+    labelnames=["datasource", "intake_key"],
+)
+
+FORWARD_EVENTS_DURATION = Histogram(
+    name="forward_events_duration",
+    documentation="Duration to collect and forward events",
+    namespace=prom_namespace,
+    labelnames=["datasource", "intake_key"],
+)
+
+
+def clear_cache(stop_event: Event):
+    while not stop_event.is_set():
+        for event_id, insertion_timestamp in EVENTS_CACHE.items():
+            if datetime.now() - insertion_timestamp < timedelta(hours=6):
+                del EVENTS_CACHE[event_id]
+        sleep(600)
+
 
 class Office365Connector(Connector):
     def __init__(
@@ -18,23 +53,93 @@ class Office365Connector(Connector):
             intake_uuid=intake_uuid,
             community_uuid=intake_community_uuid,
         )
+        self.client = Office365API(
+            client_id=self.settings.client_id,
+            client_secret=self.settings.client_secret,
+            tenant_id=self.settings.tenant_uuid,
+            publisher_id=self.settings.publisher_id,
+        )
 
-    def pull_content(self) -> list[dict]:
-        pass
+    def pull_content(self, last_pull_date: datetime) -> list[dict]:
+        pulled_events: list[dict] = []
+
+        content_types = self.client.list_subscriptions()
+        for content_type in content_types:
+            # Get the paginated contents from a subscription
+            for contents in self.client.get_subscription_contents(
+                content_type, start_time=last_pull_date, end_time=datetime.utcnow()
+            ):
+                for content in contents:
+                    events = self.client.get_content(content["contentUri"])
+                    for event in events:
+                        pulled_events.append(event)
+
+        return pulled_events
 
     def forward_events(self, events: list[dict]):
         self.log(f"Pushing {len(events)} events to intake", level="info")
+        OUTCOMING_EVENTS.labels(intake_key=self.settings.intake_key, datasource="office365").inc()
         self.push_events_to_intakes(events)
 
     def check_for_duplicates(self, events: list[dict]) -> list[dict]:
-        pass
+        deduplicated_events: list[dict] = []
+        for event in events:
+            if not self.event_in_cache(event["id"]):
+                deduplicated_events.append(event)
+                EVENTS_CACHE[event["id"]] = datetime.now()
+        return deduplicated_events
+
+    @lru_cache
+    def event_in_cache(self, event_id: str) -> bool:
+        return event_id in EVENTS_CACHE
+
+    def activate_subscriptions(self):
+        already_enabled_types = set(self.client.list_subscriptions())
+        missing_types = self.settings.content_types - already_enabled_types
+
+        enabled_types = []
+        # Activate missing types
+        for content_type in missing_types:
+            try:
+                self.client.activate_subscription(content_type)
+                enabled_types.append(content_type)
+            except FailedToActivateO365Subscription as exp:
+                self._logger.warning(
+                    "Failed to activate subscription",
+                    tenant_id=self.settings.tenant_uuid,
+                    content_type=content_type,
+                    exp=exp,
+                )
+
+        # If failed at enabling at least one format, then raise an
+        # exception.
+        if set(enabled_types) != missing_types:
+            raise FailedToActivateO365Subscription()
 
     def run(self):
+        stop_clear_cache_thread: Event = Event()
+        clear_cache_thread = Thread(target=clear_cache)
+        clear_cache_thread.start(stop_clear_cache_thread)
+
+        self.activate_subscriptions()
+
+        last_pull_date = datetime.utcnow() - timedelta(hours=3)
+
         while self.running:
-            events = self.pull_content()
+            start_time = time.time()
+
+            events = self.pull_content(last_pull_date)
             deduplicated_events = self.check_for_duplicates(events)
-            self.forward_events(events)
-            sleep(300)
-            
+            self.forward_events(deduplicated_events)
+
+            FORWARD_EVENTS_DURATION.labels(intake_key=self.settings.intake_key, datasource="office365").observe(
+                time.time() - start_time
+            )
+
+            last_pull_date = datetime.utcnow()
+            sleep(60)
+
         # Stop the connector executor
+        stop_clear_cache_thread.set()
+        clear_cache_thread.join()
         self._executor.shutdown(wait=True)
