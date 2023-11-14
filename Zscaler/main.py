b509cb9a7e7d4983ee3d5bb1feda25578a4be113
rombernier <romain.bernier@sekoia.io>
2023-11-14 16:32:09 +0100

    first try to build a module from scratch

--- Zscaler/main.py
+++ Zscaler/main.py
@@ -3,7 +3,7 @@ from sekoia_automation.module import Module
 from zscaler.block_ioc import ZscalerBlockIOC
 
 if __name__ == "__main__":
-    module = module()
+    module = Module()
     module.register(ZscalerBlockIOC, "zscaler_block_ioc")
 
     module.run()
